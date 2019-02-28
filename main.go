371e6d4a30406c4e89a792b5c6e479f11fc00f4b
ConnorJarvis <connorpjarvis@gmail.com>
2019-02-28 16:36:48 -0500

    Changed library

--- main.go
+++ main.go
@@ -1,109 +1,101 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
 	"math"
 	"time"
 
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
-	"github.com/go-vgo/robotgo"
-	"golang.org/x/image/colornames"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var board [50][50]int
 var pause bool
+var field *ebiten.Image
 
-func run() {
-	cfg := pixelgl.WindowConfig{
-		Title:  "Game of Life",
-		Bounds: pixel.R(0, 0, 800, 800),
-	}
-	win, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
-	// imd := imdraw.New(nil)
-	// color := float64(255)
-	// x, y := 1, 1
-	// imd.Color = pixel.RGB(0, 0, 0)
-	// imd.Push(pixel.V(0, 0))
-	// imd.Color = pixel.RGB(0, 0, 0)
-	// imd.Push(pixel.V(100, 100))
-	// imd.Rectangle(0)
-	go EvolveBoard()
-	go ListenForSpacebar()
-	// go ListenForMouse(win)
-	for !win.Closed() {
-		if win.JustPressed(pixelgl.MouseButtonLeft) {
-			mousePos := win.MousePosition()
-			x := (mousePos.X) / 16
-			y := (mousePos.Y) / 16
-			xInt := int(math.Floor(x))
-			yInt := int(math.Floor(y))
-			if board[49-yInt][xInt] == 0 {
-				board[49-yInt][xInt] = 1
-			} else {
-				board[49-yInt][xInt] = 0
-			}
-			time.Sleep(time.Millisecond * 100)
-		}
-		win.Clear(colornames.White)
-		// spew.Dump(board)
-		for x := 0; x < 50; x++ {
-			for y := 0; y < 50; y++ {
+var whiteSquare *ebiten.Image
+var blackSquare *ebiten.Image
+var spaceButtonLastPressed int64
+var mouseButtonLastPressed int64
+var lastEvolve int64
 
-				color := float64(0)
-				if board[x][y] == 0 {
-					color = 255
-				}
-				imd := imdraw.New(nil)
-				imd.Color = pixel.RGB(color, color, color)
-				imd.Push(pixel.V((float64(y) * 16), 800-(float64(x)*16)))
-				imd.Color = pixel.RGB(color, color, color)
-				imd.Push(pixel.V((float64(y+1) * 16), 800-(float64(x+1)*16)))
-				imd.Rectangle(0)
-				imd.Draw(win)
-			}
-		}
-
-		win.Update()
-	}
-
-}
-
-// func ListenForMouse(win *pixelgl.Window) {
-// 	for {
-// 		robotgo.AddEvent("mleft")
-
-// 	}
-
-// }
-
-func ListenForSpacebar() {
-	for {
-		robotgo.AddEvent("space")
-		if pause == false {
-			pause = true
-		} else {
-			pause = false
-		}
-		time.Sleep(time.Millisecond * 200)
-	}
-
-}
-func EvolveBoard() {
-	for {
-		time.Sleep(time.Millisecond * 250)
-		if pause == false {
+func update(screen *ebiten.Image) error {
+	if pause == false {
+		evolveTime := time.Now().UnixNano()
+		if evolveTime-lastEvolve > 250000000 {
+			lastEvolve = evolveTime
 			board = Evolve(board)
 		}
+		// board = Evolve(board)
 	}
+	screen.Fill(color.RGBA{255, 255, 255, 255})
+	field.Fill(color.RGBA{255, 255, 255, 255})
 
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		pressedTime := time.Now().UnixNano()
+		if pressedTime-spaceButtonLastPressed > 500000000 {
+			spaceButtonLastPressed = pressedTime
+			if pause == true {
+				pause = false
+			} else {
+				pause = true
+			}
+		}
+
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		pressedTime := time.Now().UnixNano()
+		if pressedTime-mouseButtonLastPressed > 400000000 {
+			mouseButtonLastPressed = pressedTime
+			mouseX, mouseY := ebiten.CursorPosition()
+			x := (mouseX) / 16
+			y := (mouseY) / 16
+			xInt := int(math.Floor(float64(x)))
+			yInt := int(math.Floor(float64(y)))
+			if board[xInt][yInt] == 0 {
+				board[yInt][xInt] = 1
+			} else {
+				board[yInt][xInt] = 0
+			}
+
+		}
+	}
+	for x := 0; x < 50; x++ {
+		for y := 0; y < 50; y++ {
+			opts := &ebiten.DrawImageOptions{}
+			opts.GeoM.Translate(float64(x)*16, float64(y)*16)
+			if board[y][x] == 1 {
+				field.DrawImage(blackSquare, opts)
+			} else {
+				field.DrawImage(whiteSquare, opts)
+			}
+
+		}
+	}
+	fps := fmt.Sprintf("TPS: %f", ebiten.CurrentTPS())
+	ebitenutil.DebugPrint(field, fps)
+	screen.DrawImage(field, nil)
+	return nil
 }
+
+// func Click() {
+
+// 	if board[49-yInt][xInt] == 0 {
+// 		board[49-yInt][xInt] = 1
+// 	} else {
+// 		board[49-yInt][xInt] = 0
+// 	}
+// 	time.Sleep(time.Millisecond * 100)
+// }
 
 func main() {
 	pause = true
+	whiteSquare, _ = ebiten.NewImage(16, 16, ebiten.FilterDefault)
+	whiteSquare.Fill(color.White)
+	blackSquare, _ = ebiten.NewImage(16, 16, ebiten.FilterDefault)
+	blackSquare.Fill(color.Black)
+	field, _ = ebiten.NewImage(800, 800, ebiten.FilterNearest)
 	board = [50][50]int{
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -156,5 +148,9 @@ func main() {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	pixelgl.Run(run)
+
+	if err := ebiten.Run(update, 800, 800, 1, "Game of Life"); err != nil {
+		panic(err)
+	}
+
 }
